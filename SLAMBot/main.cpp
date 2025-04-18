b2dcf4471a73c6c36a6fcf6205e6b9765a7881a0
Fredrik Rasmusson <rasmussonf@gmail.com>
2025-04-18 22:38:34 +0200

    Clang format.

--- SLAMBot/main.cpp
+++ SLAMBot/main.cpp
@@ -3,19 +3,17 @@
 #include <iostream>
 
 void createAndPrintMatrix() {
-    // Create a 3x3 matrix of doubles
-    Eigen::MatrixXd mat(3, 3);
+  // Create a 3x3 matrix of doubles
+  Eigen::MatrixXd mat(3, 3);
 
-    // Fill it with values
-    mat << 1, 2, 3,
-           4, 5, 6,
-           7, 8, 9;
+  // Fill it with values
+  mat << 1, 2, 3, 4, 5, 6, 7, 8, 9;
 
-    // Print the matrix
-    std::cout << "Matrix:\n" << mat << std::endl;
+  // Print the matrix
+  std::cout << "Matrix:\n" << mat << std::endl;
 }
 
 int main() {
-	createAndPrintMatrix();
-	return 0;
+  createAndPrintMatrix();
+  return 0;
 }
