4c643f7bf04f04108ae0d83e01229ba4d4a600ac
Maximilian Salén <maximilian@live.se>
2025-04-20 16:58:23 +0200

    Added opencv to build with a test file

---
+++ SLAMBot/TestOpenCV.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+
+void test_opencv() {
+    int width = 400;
+    int height = 400;
+    cv::Mat image(height, width, CV_8UC3, cv::Scalar(0, 0, 255)); // Red image (BGR format)
+
+    cv::imshow("Display Window", image);
+
+    cv::waitKey(0);
+
+}
+
+int main() {
+    test_opencv();
+    return 0;
+}
