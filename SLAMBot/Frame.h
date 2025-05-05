b6a8b70af3402664a171a4addf2a2c870b9ac171
Maximilian Salén <maximilian@live.se>
2025-05-05 22:08:28 +0200

    Completed the frame struct

--- SLAMBot/Frame.h
+++ SLAMBot/Frame.h
@@ -1,5 +1,6 @@
-#include <opencv2/opencv.hpp>
 #include <memory>
+#include <opencv2/opencv.hpp>
+#include <sophus/geometry.hpp>
 #include "Eigen/Dense"
 #pragma once
 
@@ -15,8 +16,28 @@ struct Frame {
        unsigned long m_id = 0; //ID of this frame
        unsigned long m_keyframeId = 0; //ID of keyframe
        bool isKeyframe = false;
+       Sophus::SE3d m_pose;
+
+       
 
        cv::Mat image;
+
+    public:
+        Frame() {}
+
+        Frame(long id, const Sophus::SE3d &pose, const cv::Mat image);
+
+        Sophus::SE3d pose() {
+            return m_pose;
+        }
+
+        void setPose(const Sophus::SE3d &pose) {
+            m_pose = pose;
+        }
+
+        void setKeyFrame();
+
+        static std::shared_ptr<Frame> CreateFrame();
 
 
 };    
